5a716c1f6f8fa669374f17072b7f9c9363c294c4
Dima <ifffweknow@gmail.com>
2021-07-21 16:13:17 +0300

    deploy to heroku

--- server.js
+++ server.js
@@ -1,35 +1,37 @@
-const app = require('express')();
-const server = require('http').createServer(app);
-const cors = require('cors');
+const app = require("express")();
+const server = require("http").createServer(app);
+const cors = require("cors");
+const path = require("path");
 
-const io = require('socket.io')(server, {
+const io = require("socket.io")(server, {
   cors: {
-    origin: '*',
-    methods: ['GET', 'POST'],
+    origin: "*",
+    methods: ["GET", "POST"],
   },
 });
 
 app.use(cors());
+app.user(express.static(path.join(__dirname, "build")))
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send(`Server is running`);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname+"/build/index.html"))
 });
 
-io.on('connection', (socket) => {
-  socket.emit('me', socket.id);
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
 
-  socket.on('disconnect', () => {
-    socket.broadcast.emit('callEnded');
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callEnded");
   });
 
-  socket.on('callUser', ({ userToCall, signalData, from, name }) => {
-    io.to(userToCall).emit('callUser', { signal: signalData, from, name });
+  socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit("callUser", { signal: signalData, from, name });
   });
 
-  socket.on('answerCall', (data) => {
-    io.to(data.to).emit('callAccepted', data.signal);
+  socket.on("answerCall", (data) => {
+    io.to(data.to).emit("callAccepted", data.signal);
   });
 });
 
