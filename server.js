0ca1296f61cb1242ba9a309197aa32cec169fac9
Dima <ifffweknow@gmail.com>
2021-07-21 16:50:22 +0300

    second test heroku

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const app = require("express")();
 const path = require("path");
-
-const io = require("socket.io");
-
-app.use(express.static(path.join(__dirname, "build")));
+const server = require('http').createServer(app);
+const io = require("socket.io")(server);
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+app.use(express.static(path.join(__dirname, "./build")));
+
+app.get("/", (req, res, next) => {
+  res.sendFile(path.join(__dirname + "./index.html"));
 });
 
 io.on("connection", (socket) => {
@@ -27,4 +27,4 @@ io.on("connection", (socket) => {
   });
 });
 
-app.listen(PORT, () => console.log(`Server is listeting on PORT ${PORT}`));
+server.listen(PORT, () => console.log(`Server is listeting on PORT ${PORT}`));
