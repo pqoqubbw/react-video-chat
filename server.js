a5454dceb3aa34dc2f6bfd173961716f4ffbf996
Dima <ifffweknow@gmail.com>
2021-07-21 16:42:21 +0300

    test heroku

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
 const app = require("express")();
-const server = require("http").createServer(app);
-// const cors = require("cors");
 const path = require("path");
 
-const io = require("socket.io")(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
-});
+const io = require("socket.io");
 
-// app.use(cors());
 app.use(express.static(path.join(__dirname, "build")));
 
 const PORT = process.env.PORT || 5000;
@@ -35,4 +27,4 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(PORT, () => console.log(`Server is listeting on PORT ${PORT}`));
+app.listen(PORT, () => console.log(`Server is listeting on PORT ${PORT}`));
