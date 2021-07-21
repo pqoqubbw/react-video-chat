3d60dbe7c8a786d284c9ac18e972fecfb46ef5af
Dima <ifffweknow@gmail.com>
2021-07-21 16:31:27 +0300

    fixed bugs

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ const io = require("socket.io")(server, {
 });
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, "build")))
+app.use(express.static(path.join(__dirname, "build")));
 
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"))
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 io.on("connection", (socket) => {
